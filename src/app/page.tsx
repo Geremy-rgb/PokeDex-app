518ea305a420915399c4489c7ce6e5284159eaa2
Geremy-rgb <geremydiaz2005@gmail.com>
2025-04-16 12:15:31 -0600

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,77 @@
-import Image from "next/image";
+"use client"
 
-export default function Home() {
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Link from "next/link";
+
+
+interface Pokemon {
+
+  name: string;
+  url: string;
+
+}
+
+export default function App () {
+
+  const [pokemons, setPokemons] = useState <Pokemon[]> ([]);
+  const [isLoading, setIsLoading] = useState (true);
+  const [isError, setIsError] = useState (false);
+
+  const handleGetPokemons = async() => {
+    
+      try{
+        const response = await axios.get("https://pokeapi.co/api/v2/pokemon/");
+      setPokemons(response.data.results)
+      }catch{
+        setIsError(true);
+      }finally{
+        setIsLoading(false);
+      }
+  }
+
+  useEffect (() => {
+    handleGetPokemons();
+  }, []);
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div>
+
+      <div className="mx-auto w-64 mt-10 text-4xl">
+      <h1 className="">Pokemons list</h1>
+      </div>
+      
+      <div className="grid grid-cols-4 gap-2 ml-3.5">
+
+        {isLoading ? <h1>cargando</h1> : null}
+
+        {pokemons.map((pokemon, index) => (
+
+          <Link href={`/pokemon/${index + 1}`} key={pokemon.name}>
+
+              <div className="text-3xl text-center font-semibold" key={`${pokemon}-${index}`}>
+
+             <img className="max-w-96" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${
+             index + 1}.png`}/>
+
+             <h1>{pokemon.name}</h1>
+             <h2>{(index+1).toString().padStart(3, "0")}</h2>
+
+            </div> 
+          
+          </Link>
+
+            
+        ))}
+      </div>
+
+      {isError ? <h1>Ocurrió un error</h1> : null}
+
     </div>
-  );
+
+  )
+
+
+
 }
