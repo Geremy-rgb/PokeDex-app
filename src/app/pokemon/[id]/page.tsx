5a2e0b29e6bb602e25710b6b87b0f08db14eb9fa
Geremy-rgb <geremydiaz2005@gmail.com>
2025-04-16 20:08:21 -0600

    update

--- src/app/pokemon/[id]/page.tsx
+++ src/app/pokemon/[id]/page.tsx
@@ -1,30 +1,19 @@
-
-interface PokemonType {
-  slot: number;
-  type: {
-    name: string;
-    url: string;
-  };
-}
-
 interface Pokemon {
   id: number;
   name: string;
-  types: PokemonType[];
+  types: {
+    type: { name: string };
+  }[];
   sprites: {
     other: {
-      ["official-artwork"]: {
+      ['official-artwork']: {
         front_default: string;
       };
     };
   };
 }
 
-export default async function PokemonPage({
-  params,
-}: {
-  params: { id: string };
-}) {
+export default async function PokemonPage({ params }: { params: { id: string } }) {
   const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${params.id}`);
   const pokemon: Pokemon = await res.json();
 
@@ -32,14 +21,12 @@ export default async function PokemonPage({
     <div className="text-center mt-10">
       <h1 className="text-3xl font-bold capitalize">{pokemon.name}</h1>
       <img
-        src={pokemon.sprites.other["official-artwork"].front_default}
+        src={pokemon.sprites.other['official-artwork'].front_default}
         alt={`Imagen de ${pokemon.name}`}
         className="w-64 mx-auto"
       />
       <p className="mt-4">ID: {pokemon.id}</p>
-      <p>
-        Tipo(s): {pokemon.types.map((t) => t.type.name).join(", ")}
-      </p>
+      <p>Tipo(s): {pokemon.types.map((t) => t.type.name).join(", ")}</p>
     </div>
   );
 }
