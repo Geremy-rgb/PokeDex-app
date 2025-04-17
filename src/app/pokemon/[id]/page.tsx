61ea3398092954f00f33d2b22759829467b8e61b
Geremy-rgb <geremydiaz2005@gmail.com>
2025-04-16 20:15:44 -0600

    update

--- src/app/pokemon/[id]/page.tsx
+++ src/app/pokemon/[id]/page.tsx
@@ -13,20 +13,23 @@ interface Pokemon {
   };
 }
 
-export default async function PokemonPage({ params }: { params: { id: string } }) {
-  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${params.id}`);
+export default async function PokemonPage({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
   const pokemon: Pokemon = await res.json();
 
   return (
     <div className="text-center mt-10">
       <h1 className="text-3xl font-bold capitalize">{pokemon.name}</h1>
       <img
-        src={pokemon.sprites.other['official-artwork'].front_default}
+        src={pokemon.sprites.other["official-artwork"].front_default}
         alt={`Imagen de ${pokemon.name}`}
         className="w-64 mx-auto"
       />
       <p className="mt-4">ID: {pokemon.id}</p>
-      <p>Tipo(s): {pokemon.types.map((t) => t.type.name).join(", ")}</p>
+      <p>
+        Tipo(s): {pokemon.types.map((t) => t.type.name).join(", ")}
+      </p>
     </div>
   );
 }
